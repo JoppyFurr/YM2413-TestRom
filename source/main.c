25ae21e864bb35f251bb38534fee2ea506ec018f
Joppy Furr <joppyfurr@gmail.com>
2023-11-12 00:41:58 +1300

    Improve key spacing, allow keys to be drawn activated

--- source/main.c
+++ source/main.c
@@ -28,47 +28,131 @@ static void clear_background (void)
 }
 
 
+const uint8_t keyboard_upper_active [12] = {
+    PATTERN_KEYS + 10, PATTERN_KEYS + 18, PATTERN_KEYS + 12, PATTERN_KEYS + 18,
+    PATTERN_KEYS + 12, PATTERN_KEYS + 10, PATTERN_KEYS + 18, PATTERN_KEYS + 12,
+    PATTERN_KEYS + 18, PATTERN_KEYS + 12, PATTERN_KEYS + 18, PATTERN_KEYS + 12
+};
+
+const uint8_t keyboard_upper_inactive [12] = {
+    PATTERN_KEYS + 2, PATTERN_KEYS + 8, PATTERN_KEYS + 4, PATTERN_KEYS + 8,
+    PATTERN_KEYS + 4, PATTERN_KEYS + 2, PATTERN_KEYS + 8, PATTERN_KEYS + 4,
+    PATTERN_KEYS + 8, PATTERN_KEYS + 4, PATTERN_KEYS + 8, PATTERN_KEYS + 4
+};
+
+const uint8_t keyboard_mid_active [12] = {
+    PATTERN_KEYS + 10, PATTERN_KEYS + 20, PATTERN_KEYS + 12, PATTERN_KEYS + 20,
+    PATTERN_KEYS + 12, PATTERN_KEYS + 10, PATTERN_KEYS + 20, PATTERN_KEYS + 12,
+    PATTERN_KEYS + 20, PATTERN_KEYS + 12, PATTERN_KEYS + 20, PATTERN_KEYS + 12
+};
+
+const uint8_t keyboard_lower_inactive [12] = {
+    PATTERN_KEYS + 2, PATTERN_KEYS + 3, PATTERN_KEYS + 4, PATTERN_KEYS + 5,
+    PATTERN_KEYS + 4, PATTERN_KEYS + 2, PATTERN_KEYS + 6, PATTERN_KEYS + 4,
+    PATTERN_KEYS + 7, PATTERN_KEYS + 4, PATTERN_KEYS + 5, PATTERN_KEYS + 4
+};
+
+const bool key_extends_left [12] = {
+    false, false, true, false, true,
+    false, false, true, false, true, false, true
+};
+
+const bool key_extends_right [12] = {
+    true, false, true, false, false,
+    true, false, true, false, true, false, false
+};
+
+
 /*
- * Get the appropriate tile for a key on the keyboard.
+ * Update the display of a key to be either active or inactive.
+ * Assumes that only one key will ever be active at a time.
  */
-static uint8_t keyboard_tile (uint8_t key, bool top)
+static void keyboard_update (uint8_t key, bool active)
 {
-    if (top)
+    uint8_t key_tile;
+
+    /* Top section */
+    if (active)
     {
-        switch (key % 12)
-        {
-            case 0:     case 5:
-                return PATTERN_KEYS + 2;
-
-            case 1:     case 3:
-            case 6:     case 8:
-            case 10:
-                return PATTERN_KEYS + 7;
-
-            default:
-                return PATTERN_KEYS + 3;
-        }
+        key_tile = keyboard_upper_active [key % 12];
     }
     else
     {
-        switch (key % 12)
+        key_tile = keyboard_upper_inactive [key % 12];
+    }
+    SMS_setTileatXY (key + 2, 17, key_tile);
+    SMS_setTileatXY (key + 2, 18, key_tile);
+
+    /* Mid section */
+    if (active)
+    {
+        key_tile = keyboard_mid_active [key % 12];
+    }
+    SMS_setTileatXY (key + 2, 19, key_tile);
+
+    /* Bottom section */
+    if (key_extends_left [key % 12] || key_extends_right [key % 12])
+    {
+        if (active)
         {
-            case 0:     case 5:
-                return PATTERN_KEYS + 2;
+            switch (key % 12)
+            {
+                case 0:
+                    SMS_setTileatXY (key + 2, 20, PATTERN_KEYS + 10);
+                    SMS_setTileatXY (key + 2, 21, PATTERN_KEYS + 10);
+                    SMS_setTileatXY (key + 3, 20, PATTERN_KEYS + 11);
+                    SMS_setTileatXY (key + 3, 21, PATTERN_KEYS + 11);
+                    break;
+                case 2:
+                    SMS_setTileatXY (key + 1, 20, PATTERN_KEYS + 19);
+                    SMS_setTileatXY (key + 1, 21, PATTERN_KEYS + 19);
+                    SMS_setTileatXY (key + 2, 20, PATTERN_KEYS + 12);
+                    SMS_setTileatXY (key + 2, 21, PATTERN_KEYS + 12);
+                    SMS_setTileatXY (key + 3, 20, PATTERN_KEYS + 13);
+                    SMS_setTileatXY (key + 3, 21, PATTERN_KEYS + 13);
+                    break;
+                case 4:
+                case 11:
+                    SMS_setTileatXY (key + 1, 20, PATTERN_KEYS + 21);
+                    SMS_setTileatXY (key + 1, 21, PATTERN_KEYS + 21);
+                    SMS_setTileatXY (key + 2, 20, PATTERN_KEYS + 12);
+                    SMS_setTileatXY (key + 2, 21, PATTERN_KEYS + 12);
+                    break;
+                case 5:
+                    SMS_setTileatXY (key + 2, 20, PATTERN_KEYS + 10);
+                    SMS_setTileatXY (key + 2, 21, PATTERN_KEYS + 10);
+                    SMS_setTileatXY (key + 3, 20, PATTERN_KEYS + 14);
+                    SMS_setTileatXY (key + 3, 21, PATTERN_KEYS + 14);
+                    break;
+                case 7:
+                    SMS_setTileatXY (key + 1, 20, PATTERN_KEYS + 22);
+                    SMS_setTileatXY (key + 1, 21, PATTERN_KEYS + 22);
+                    SMS_setTileatXY (key + 2, 20, PATTERN_KEYS + 12);
+                    SMS_setTileatXY (key + 2, 21, PATTERN_KEYS + 12);
+                    SMS_setTileatXY (key + 3, 20, PATTERN_KEYS + 15);
+                    SMS_setTileatXY (key + 3, 21, PATTERN_KEYS + 15);
+                    break;
+                case 9:
+                    SMS_setTileatXY (key + 1, 20, PATTERN_KEYS + 23);
+                    SMS_setTileatXY (key + 1, 21, PATTERN_KEYS + 23);
+                    SMS_setTileatXY (key + 2, 20, PATTERN_KEYS + 12);
+                    SMS_setTileatXY (key + 2, 21, PATTERN_KEYS + 12);
+                    SMS_setTileatXY (key + 3, 20, PATTERN_KEYS + 13);
+                    SMS_setTileatXY (key + 3, 21, PATTERN_KEYS + 13);
+                    break;
+            }
+        }
+        else
+        {
+            uint8_t from = ((key_extends_left  [key % 12]) ? key - 1 : key) + 2;
+            uint8_t to   = ((key_extends_right [key % 12]) ? key + 1 : key) + 2;
 
-            case 1:     case 6:
-                return PATTERN_KEYS + 4;
-
-            case 2:     case 4:
-            case 7:     case 9:
-            case 11:
-                return PATTERN_KEYS + 3;
-
-            case 3:     case 10:
-                return PATTERN_KEYS + 5;
-
-            case 8:
-                return PATTERN_KEYS + 6;
+            for (uint8_t col = from; col <= to; col++)
+            {
+                key_tile = keyboard_lower_inactive [(col - 2) % 12];
+                SMS_setTileatXY (col, 20, key_tile);
+                SMS_setTileatXY (col, 21, key_tile);
+            }
         }
     }
 }
@@ -87,27 +171,27 @@ static void draw_keyboard (void)
         SMS_setTileatXY (col, 16, PATTERN_KEYS + 0);
 
         /* Top section of keys */
-        key_tile = keyboard_tile (col - 2, true);
+        key_tile = keyboard_upper_inactive [(col - 2) % 12];
         SMS_setTileatXY (col, 17, key_tile);
         SMS_setTileatXY (col, 18, key_tile);
         SMS_setTileatXY (col, 19, key_tile);
 
         /* Bottom section of keys */
-        key_tile = keyboard_tile (col - 2, false);
+        key_tile = keyboard_lower_inactive [(col - 2) % 12];
         SMS_setTileatXY (col, 20, key_tile);
         SMS_setTileatXY (col, 21, key_tile);
 
         /* Bottom outline */
-        SMS_setTileatXY (col, 22, PATTERN_KEYS + 14);
+        SMS_setTileatXY (col, 22, PATTERN_KEYS + 16);
     }
 
     /* Right outline */
     SMS_setTileatXY (31, 16, PATTERN_KEYS + 1);
     for (uint8_t row = 17; row <= 21; row++)
     {
-        SMS_setTileatXY (31, row, PATTERN_KEYS + 8);
+        SMS_setTileatXY (31, row, PATTERN_KEYS + 9);
     }
-    SMS_setTileatXY (31, 22, PATTERN_KEYS + 15);
+    SMS_setTileatXY (31, 22, PATTERN_KEYS + 17);
 }
 
 
@@ -122,16 +206,37 @@ void main (void)
     SMS_setBackdropColor (0);
 
     SMS_loadTiles (patterns, 0, sizeof (patterns));
-    SMS_setBGScrollX (-4);
+    SMS_setBGScrollX (0xfc);
     clear_background ();
     draw_keyboard ();
 
     SMS_displayOn ();
 
     /* Main loop */
+    uint8_t frame_count = 0;
+    uint8_t current_key = 0;
     while (true)
     {
-        /* Nothing to do yet */
+        /* Basic test for key down indication - cycle through the keys. */
+        SMS_waitForVBlank ();
+        frame_count++;
+
+        if (frame_count == 30)
+        {
+            keyboard_update (current_key, true);
+        }
+
+        if (frame_count == 60)
+        {
+            keyboard_update (current_key, false);
+            frame_count = 0;
+            current_key++;
+            if (current_key > 28)
+            {
+                current_key = 0;
+            }
+        }
+
     }
 }
 

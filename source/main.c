0051dd69db42c76311f9a2c81c19d562e136a2f3
Joppy Furr <joppyfurr@gmail.com>
2023-11-14 23:38:14 +1300

    Variable height cursor

--- source/main.c
+++ source/main.c
@@ -59,6 +59,7 @@ typedef struct gui_element_s {
 
     uint8_t x;
     uint8_t y;
+    uint8_t height;
 
     element_id_t up;
     element_id_t down;
@@ -69,137 +70,137 @@ typedef struct gui_element_s {
 
 gui_element_t main_gui [ELEMENT_COUNT] = {
     [ELEMENT_INSTRUMENT] = {
-        .type = TYPE_VALUE, .max = 15, .x = 4, .y = 1,
+        .type = TYPE_VALUE, .max = 15, .x = 4, .y = 1, .height = 3,
         .up   = ELEMENT_INSTRUMENT,         .down  = ELEMENT_MOD_MULTI,
         .left = ELEMENT_INSTRUMENT,         .right = ELEMENT_VOLUME
     },
     [ELEMENT_VOLUME] = {
-        .type = TYPE_VALUE, .max = 15, .x = 7, .y = 1,
+        .type = TYPE_VALUE, .max = 15, .x = 7, .y = 1, .height = 3,
         .up   = ELEMENT_VOLUME,             .down  = ELEMENT_MOD_KSR,
         .left = ELEMENT_INSTRUMENT,         .right = ELEMENT_SUSTAIN
     },
     [ELEMENT_SUSTAIN] = {
-        .type = TYPE_LED, .max = 1, .x = 10, .y = 2,
+        .type = TYPE_LED, .max = 1, .x = 10, .y = 2, .height = 2,
         .up   = ELEMENT_SUSTAIN,            .down  = ELEMENT_MOD_VIBRATO,
         .left = ELEMENT_VOLUME,             .right = ELEMENT_FEEDBACK
     },
     [ELEMENT_FEEDBACK] = {
-        .type = TYPE_VALUE, .max = 7, .x = 25, .y = 1,
+        .type = TYPE_VALUE, .max = 7, .x = 25, .y = 1, .height = 4,
         .up   = ELEMENT_FEEDBACK,           .down  = ELEMENT_MOD_SUSTAIN_LEVEL,
         .left = ELEMENT_SUSTAIN,            .right = ELEMENT_TOTAL_LEVEL
     },
     [ELEMENT_TOTAL_LEVEL] = {
-        .type = TYPE_VALUE, .max = 63, .x = 28, .y = 1,
+        .type = TYPE_VALUE, .max = 63, .x = 28, .y = 1, .height = 4,
         .up   = ELEMENT_TOTAL_LEVEL,        .down  = ELEMENT_MOD_RELEASE_RATE,
         .left = ELEMENT_FEEDBACK,           .right = ELEMENT_TOTAL_LEVEL
     },
     [ELEMENT_MOD_MULTI] = {
-        .type = TYPE_VALUE, .max = 15, .x = 4, .y = 6,
+        .type = TYPE_VALUE, .max = 15, .x = 4, .y = 6, .height = 3,
         .up   = ELEMENT_INSTRUMENT,         .down  = ELEMENT_CAR_MULTI,
         .left = ELEMENT_MOD_MULTI,          .right = ELEMENT_MOD_KSR
     },
     [ELEMENT_MOD_KSR] = {
-        .type = TYPE_LED, .max = 1, .x = 7, .y = 6,
+        .type = TYPE_LED, .max = 1, .x = 7, .y = 6, .height = 2,
         .up   = ELEMENT_VOLUME,             .down  = ELEMENT_MOD_ENVELOPE_TYPE,
         .left = ELEMENT_MOD_MULTI,          .right = ELEMENT_MOD_VIBRATO
     },
     [ELEMENT_MOD_VIBRATO] = {
-        .type  = TYPE_LED, .max = 1, .x = 10, .y = 6,
+        .type  = TYPE_LED, .max = 1, .x = 10, .y = 6, .height = 2,
         .up    = ELEMENT_SUSTAIN,           .down  = ELEMENT_MOD_AM,
         .left  = ELEMENT_MOD_KSR,           .right = ELEMENT_MOD_WAVEFORM
     },
     [ELEMENT_MOD_WAVEFORM] = {
-        .type = TYPE_LED, .max = 1, .x = 13, .y = 6,
+        .type = TYPE_LED, .max = 1, .x = 13, .y = 6, .height = 3,
         .up   = ELEMENT_SUSTAIN,            .down  = ELEMENT_CAR_WAVEFORM,
         .left = ELEMENT_MOD_VIBRATO,        .right = ELEMENT_MOD_KSL
     },
     [ELEMENT_MOD_ENVELOPE_TYPE] = {
-        .type = TYPE_LED, .max = 1, .x = 7, .y = 8,
+        .type = TYPE_LED, .max = 1, .x = 7, .y = 8, .height = 2,
         .up   = ELEMENT_MOD_KSR,            .down  = ELEMENT_CAR_KSR,
         .left = ELEMENT_MOD_MULTI,          .right = ELEMENT_MOD_AM
     },
     [ELEMENT_MOD_AM] = {
-        .type = TYPE_LED, .max = 1, .x = 10, .y = 8,
+        .type = TYPE_LED, .max = 1, .x = 10, .y = 8, .height = 2,
         .up   = ELEMENT_MOD_VIBRATO,        .down  = ELEMENT_CAR_VIBRATO,
         .left = ELEMENT_MOD_ENVELOPE_TYPE,  .right = ELEMENT_MOD_WAVEFORM
     },
     [ELEMENT_MOD_KSL] = {
-        .type = TYPE_VALUE, .max = 3, .x = 16, .y = 6,
+        .type = TYPE_VALUE, .max = 3, .x = 16, .y = 6, .height = 3,
         .up   = ELEMENT_SUSTAIN,            .down  = ELEMENT_CAR_KSL,
         .left = ELEMENT_MOD_WAVEFORM,       .right = ELEMENT_MOD_ATTACK_RATE
     },
     [ELEMENT_MOD_ATTACK_RATE] = {
-        .type = TYPE_VALUE, .max = 15, .x = 19, .y = 6,
+        .type = TYPE_VALUE, .max = 15, .x = 19, .y = 6, .height = 3,
         .up   = ELEMENT_FEEDBACK,           .down  = ELEMENT_CAR_ATTACK_RATE,
         .left = ELEMENT_MOD_KSL,            .right = ELEMENT_MOD_DECAY_RATE
     },
     [ELEMENT_MOD_DECAY_RATE] = {
-        .type = TYPE_VALUE, .max = 15, .x = 22, .y = 6,
+        .type = TYPE_VALUE, .max = 15, .x = 22, .y = 6, .height = 3,
         .up   = ELEMENT_FEEDBACK,           .down  = ELEMENT_CAR_DECAY_RATE,
         .left = ELEMENT_MOD_ATTACK_RATE,    .right = ELEMENT_MOD_SUSTAIN_LEVEL
     },
     [ELEMENT_MOD_SUSTAIN_LEVEL] = {
-        .type = TYPE_VALUE, .max = 15, .x = 25, .y = 6,
+        .type = TYPE_VALUE, .max = 15, .x = 25, .y = 6, .height = 3,
         .up   = ELEMENT_FEEDBACK,           .down  = ELEMENT_CAR_SUSTAIN_LEVEL,
         .left = ELEMENT_MOD_DECAY_RATE,     .right = ELEMENT_MOD_RELEASE_RATE
     },
     [ELEMENT_MOD_RELEASE_RATE] = {
-        .type = TYPE_VALUE, .max = 15, .x = 28, .y = 6,
+        .type = TYPE_VALUE, .max = 15, .x = 28, .y = 6, .height = 3,
         .up   = ELEMENT_TOTAL_LEVEL,        .down  = ELEMENT_CAR_RELEASE_RATE,
         .left = ELEMENT_MOD_SUSTAIN_LEVEL,  .right = ELEMENT_MOD_RELEASE_RATE
     },
     [ELEMENT_CAR_MULTI] = {
-        .type = TYPE_VALUE, .max = 15, .x = 4, .y = 11,
+        .type = TYPE_VALUE, .max = 15, .x = 4, .y = 11, .height = 3,
         .up   = ELEMENT_MOD_MULTI,          .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_CAR_MULTI,          .right = ELEMENT_CAR_KSR
     },
     [ELEMENT_CAR_KSR] = {
-        .type = TYPE_LED, .max = 1, .x = 7, .y = 11,
+        .type = TYPE_LED, .max = 1, .x = 7, .y = 11, .height = 2,
         .up   = ELEMENT_MOD_ENVELOPE_TYPE,  .down  = ELEMENT_CAR_ENVELOPE_TYPE,
         .left = ELEMENT_CAR_MULTI,          .right = ELEMENT_CAR_VIBRATO
     },
     [ELEMENT_CAR_VIBRATO] = {
-        .type = TYPE_LED, .max = 1, .x = 10, .y = 11,
+        .type = TYPE_LED, .max = 1, .x = 10, .y = 11, .height = 2,
         .up   = ELEMENT_MOD_AM,             .down  = ELEMENT_CAR_AM,
         .left = ELEMENT_CAR_KSR,            .right = ELEMENT_CAR_WAVEFORM
     },
     [ELEMENT_CAR_WAVEFORM] = {
-        .type = TYPE_LED, .max = 1, .x = 13, .y = 11,
+        .type = TYPE_LED, .max = 1, .x = 13, .y = 11, .height = 3,
         .up   = ELEMENT_MOD_WAVEFORM,       .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_CAR_VIBRATO,        .right = ELEMENT_CAR_KSL
     },
     [ELEMENT_CAR_ENVELOPE_TYPE] = {
-        .type = TYPE_LED, .max = 1, .x = 7, .y = 13,
+        .type = TYPE_LED, .max = 1, .x = 7, .y = 13, .height = 2,
         .up   = ELEMENT_CAR_KSR,            .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_CAR_MULTI,          .right = ELEMENT_CAR_AM
     },
     [ELEMENT_CAR_AM] = {
-        .type = TYPE_LED, .max = 1, .x = 10, .y = 13,
+        .type = TYPE_LED, .max = 1, .x = 10, .y = 13, .height = 2,
         .up   = ELEMENT_CAR_VIBRATO,        .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_CAR_ENVELOPE_TYPE, .right = ELEMENT_CAR_WAVEFORM
     },
     [ELEMENT_CAR_KSL] = {
-        .type = TYPE_VALUE, .max = 3, .x = 16, .y = 11,
+        .type = TYPE_VALUE, .max = 3, .x = 16, .y = 11, .height = 3,
         .up   = ELEMENT_MOD_KSL,            .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_CAR_WAVEFORM,       .right = ELEMENT_CAR_ATTACK_RATE
     },
     [ELEMENT_CAR_ATTACK_RATE] = {
-        .type = TYPE_VALUE, .max = 15, .x = 19, .y = 11,
+        .type = TYPE_VALUE, .max = 15, .x = 19, .y = 11, .height = 3,
         .up   = ELEMENT_MOD_ATTACK_RATE,    .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_CAR_KSL,            .right = ELEMENT_CAR_DECAY_RATE
     },
     [ELEMENT_CAR_DECAY_RATE] = {
-        .type = TYPE_VALUE, .max = 15, .x = 22, .y = 11,
+        .type = TYPE_VALUE, .max = 15, .x = 22, .y = 11, .height = 3,
         .up   = ELEMENT_MOD_DECAY_RATE,     .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_CAR_ATTACK_RATE,    .right = ELEMENT_CAR_SUSTAIN_LEVEL
     },
     [ELEMENT_CAR_SUSTAIN_LEVEL] = {
-        .type = TYPE_VALUE, .max = 15, .x = 25, .y = 11,
+        .type = TYPE_VALUE, .max = 15, .x = 25, .y = 11, .height = 3,
         .up   = ELEMENT_MOD_SUSTAIN_LEVEL, .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_CAR_DECAY_RATE,     .right = ELEMENT_CAR_RELEASE_RATE
     },
     [ELEMENT_CAR_RELEASE_RATE] = {
-        .type = TYPE_VALUE, .max = 15, .x = 28, .y = 11,
+        .type = TYPE_VALUE, .max = 15, .x = 28, .y = 11, .height = 3,
         .up   = ELEMENT_MOD_RELEASE_RATE,   .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_CAR_SUSTAIN_LEVEL,  .right = ELEMENT_CAR_RELEASE_RATE
     }
@@ -209,7 +210,7 @@ gui_element_t main_gui [ELEMENT_COUNT] = {
 /*
  * Update the cursor position.
  */
-void cursor_update (uint8_t x, uint8_t y)
+void cursor_update (uint8_t x, uint8_t y, uint8_t height)
 {
     x = (x << 3) - 8;
     y = (y << 3) - 4;
@@ -220,11 +221,16 @@ void cursor_update (uint8_t x, uint8_t y)
     SMS_addSprite (x,      y,      PATTERN_CURSOR + 0);
     SMS_addSprite (x +  8, y,      PATTERN_CURSOR + 1);
     SMS_addSprite (x + 16, y,      PATTERN_CURSOR + 2);
-    SMS_addSprite (x,      y +  8, PATTERN_CURSOR + 3);
-    SMS_addSprite (x + 16, y +  8, PATTERN_CURSOR + 5);
-    SMS_addSprite (x,      y + 16, PATTERN_CURSOR + 6);
-    SMS_addSprite (x +  8, y + 16, PATTERN_CURSOR + 7);
-    SMS_addSprite (x + 16, y + 16, PATTERN_CURSOR + 8);
+
+    for (uint8_t i = 1; i < height; i++)
+    {
+        SMS_addSprite (x,      y + (i << 3), PATTERN_CURSOR + 3);
+        SMS_addSprite (x + 16, y + (i << 3), PATTERN_CURSOR + 5);
+    }
+
+    SMS_addSprite (x,      y + (height << 3), PATTERN_CURSOR + 6);
+    SMS_addSprite (x +  8, y + (height << 3), PATTERN_CURSOR + 7);
+    SMS_addSprite (x + 16, y + (height << 3), PATTERN_CURSOR + 8);
 
     SMS_copySpritestoSAT ();
 }
@@ -267,7 +273,8 @@ void main (void)
 
     element_id_t current_element = ELEMENT_INSTRUMENT;
     cursor_update (main_gui [current_element].x,
-                   main_gui [current_element].y);
+                   main_gui [current_element].y,
+                   main_gui [current_element].height);
 
     SMS_displayOn ();
 
@@ -300,7 +307,8 @@ void main (void)
         }
         /* TODO: Only update when needed */
         cursor_update (main_gui [current_element].x,
-                       main_gui [current_element].y);
+                       main_gui [current_element].y,
+                       main_gui [current_element].height);
 
         /* DEBUG - Cycle through the keys. */
         frame_count++;
